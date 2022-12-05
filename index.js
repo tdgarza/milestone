2467d85b9b1a64a6ff8bf2a4a77aa8b999363ea5
Tomas Garza Moya <tomasgarzamoya@Tomass-MacBook-Air.local>
2022-12-04 18:51:11 -0600

    Primera practica de backend controlando los vinculos

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 router.get("/", (req, res) =>{
-    res,sendFile(path.join(__dirname + 'templates/index.html'))
+    res.sendFile(path.join(__dirname + '/templates/index.html'))
+});
+router.get("/contacto", (req, res) => {
+    res.sendFile(path.join(__dirname + '/templates/contacto.html'))
+});
+router.get("/perfil", (req, res) => {
+    res.sendFile(path.join(__dirname + '/templates/perfil.html'))
 });
 
+router.post("/", (req, res) => {
+    res.send("El usuario " + req.body.first_name + " ha sido registrado.");
+})
 app.use("/", router);
 app.listen(process.env.port || 8080);
 
